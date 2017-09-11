d47b65528161efc5a74da384f865592adfbf68e9
Alex <jerk401@gmail.com>
2017-09-12 02:26:39 +0300

    PoC parameterised injections

--- simple-injector/src/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app/app");
-var container_1 = require("./lib/container");
-var providers_1 = require("./app/providers");
-container_1.container.register(providers_1.APP_PROVIDERS);
-var app = container_1.container.resolve(app_1.App);
-app.run();
-//# sourceMappingURL=client.js.map
