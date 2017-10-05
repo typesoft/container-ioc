52e131dcc5e29f1969c0f197fb411d9d560cb649
Alex <jerk401@gmail.com>
2017-10-06 02:18:38 +0300

    examples cleanup

--- examples/typescript/provider-shortcut-for-classes.ts
+++
@@ -1,8 +0,0 @@
-import { Container, Injectable } from 'container-ioc';
-
-@Injectable() class App {}
-const container = new Container();
-
-container.register(App); // the same as { token: App, useClass: App }
-
-const app = container.resolve(App);
