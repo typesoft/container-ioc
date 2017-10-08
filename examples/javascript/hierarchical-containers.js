f3b983836af37370aa6da96542a5abba11dd8b6c
Alex <jerk401@gmail.com>
2017-10-08 20:03:37 +0300

    updated examples

--- examples/javascript/hierarchical-containers.js
+++ examples/javascript/hierarchical-containers.js
@@ -1,12 +1,20 @@
 import { Container } from 'container-ioc';
 
+/*
+    If a container can't find a value within itself, it will look it up in ascendant containers.
+    There a 3 ways to set a parent for a container:
+ */
+
 const grandParentContainer = new Container();
 
+// 1.
 const parentContainer = grandParentContainer.createChild();
 
+// 2.
 const childContainer = new Container();
 childContainer.setParent(parentContainer);
 
+// 3.
 const deepNestedContainer = new Container({
     parent: childContainer
 });
