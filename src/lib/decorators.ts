e1ba88ae81d9e312ebb32184a8d4a2f33de64b2d
Dmitrijs Minajevs <dmitrijs.minajevs@outlook.com>
2018-10-02 16:21:30 +0300

    Simplify dependency injection syntax in constructor with Reflect API

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -2,6 +2,7 @@ import { IInjectionMd, ProviderToken } from './interfaces';
 import { INJECTABLE_MD_KEY, INJECTIONS_MD_KEY } from './metadata/keys';
 import { IMetadataAnnotator } from './metadata/metadata-annotator.interface';
 import { AnnotatorProvider } from './metadata/index';
+import 'reflect-metadata';
 
 const MetadataAnnotator: IMetadataAnnotator = AnnotatorProvider.get();
 
@@ -12,6 +13,17 @@ export function Injectable(injections?: ProviderToken[]) {
         if (injections && Array.isArray(injections)) {
             const injectionsMd: IInjectionMd[] = MetadataAnnotator.getMetadata(INJECTIONS_MD_KEY, target) || [];
             injections.forEach(token => injectionsMd.push(token));
+            MetadataAnnotator.defineMetadata(INJECTIONS_MD_KEY, injectionsMd, target);
+        }
+
+        const parameters = Reflect.getMetadata('design:paramtypes', target);
+        if (parameters && Array.isArray(parameters)) {
+            const injectionsMd: IInjectionMd[] = MetadataAnnotator.getMetadata(INJECTIONS_MD_KEY, target) || [];
+            parameters.forEach((param, index) => {
+                if (param['DI:injectable'] && !injectionsMd[index]) {
+                    injectionsMd[index] = param;
+                }
+            });
             MetadataAnnotator.defineMetadata(INJECTIONS_MD_KEY, injectionsMd, target);
         }
     };
